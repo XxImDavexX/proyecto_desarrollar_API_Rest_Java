92e20088a00bad8ba84afd168e2c4b2e72526753
XxImDavexX <david411dark@gmail.com>
2024-07-03 08:32:15 -0600

    se corrigio la ruta de DatosDireccion

--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,10 +1,10 @@
 package med.voll.api.medico;
 
-import med.voll.api.DatosDireccion;
+import med.voll.api.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(String nombre,
                                   String email,
                                   String documento,
-                                  Especialidad especialidad,
-                                  DatosDireccion direccion) /*Se utilizara un enum por que en el modelo es un dropdown con 4 opciones)*/ {
+                                  Especialidad especialidad /*Se utilizara un enum por que en el modelo es un dropdown con 4 opciones)*/,
+                                  DatosDireccion direccion)  {
 }
